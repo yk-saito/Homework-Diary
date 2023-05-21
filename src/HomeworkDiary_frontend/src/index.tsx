dc9cf36e52b8f7b7e0e5089d5ef0539fb3879386
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-05-21 16:39:01 +0900

    chore: setup chakra-ui

--- src/HomeworkDiary_frontend/src/index.tsx
+++ src/HomeworkDiary_frontend/src/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
 
-root.render(<App />);
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
